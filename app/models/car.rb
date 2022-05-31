f90346ce6150f81c4f1d019ce7cc30a4f772f3c7
Iliya Oblakov <iliyaoblakov@gmail.com>
2022-05-31 11:28:54 +0100

    validations done

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,8 @@
 class Car < ApplicationRecord
   belongs_to :user
+  validates :make, presence: true
+  validates :momdel, presence: true
+  validates :photo_url, presence: true
+  validates :price, presence: true
+  validates :location, presence: true
 end
