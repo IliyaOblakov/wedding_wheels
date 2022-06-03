68dce5ca6321a57a4cd46290118723b4457d6106
Iliya Oblakov <iliyaoblakov@gmail.com>
2022-06-03 15:59:03 +0100

    done

--- app/javascript/controllers/scroll_down_controller.js
+++ app/javascript/controllers/scroll_down_controller.js
@@ -3,13 +3,14 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="scroll-down"
 export default class extends Controller {
   connect() {
-    console.log('hello from controller scroll')
+
     // const element = document.getElementById("scroll-point")
     // e.preventDefault()
 
 
   }
    scroll(event) {
+     console.log(this.element)
      event.preventDefault()
     scrollBy(0, 1000)
   }
